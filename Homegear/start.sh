22e8b32e56576d4413dbd918f7c5f40a569618f2
Jan Schulz-Hofen <jan@plan.io>
2017-11-22 21:41:16 +0100

    adding Homegear

---
+++ Homegear/start.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+if ! [ "$(ls -A /etc/homegear)" ]; then
+	cp -R /etc/homegear.config/* /etc/homegear/
+fi
+
+if ! [ "$(ls -A /var/lib/homegear)" ]; then
+	cp -a /var/lib/homegear.data/* /var/lib/homegear/
+else
+	rm -Rf /var/lib/homegear/modules/*
+	rm -Rf /var/lib/homegear/flows/nodes/*
+	cp -a /var/lib/homegear.data/modules/* /var/lib/homegear/modules/
+	cp -a /var/lib/homegear.data/flows/nodes/* /var/lib/homegear/flows/nodes/
+fi
+
+if ! [ -f /etc/homegear/dh1024.pem ]; then
+	openssl genrsa -out /etc/homegear/homegear.key 2048
+	openssl req -batch -new -key /etc/homegear/homegear.key -out /etc/homegear/homegear.csr
+	openssl x509 -req -in /etc/homegear/homegear.csr -signkey /etc/homegear/homegear.key -out /etc/homegear/homegear.crt
+	rm /etc/homegear/homegear.csr
+	chown homegear:homegear /etc/homegear/homegear.key
+	chmod 400 /etc/homegear/homegear.key
+	openssl dhparam -check -text -5 -out /etc/homegear/dh1024.pem 1024
+	chown homegear:homegear /etc/homegear/dh1024.pem
+	chmod 400 /etc/homegear/dh1024.pem
+fi
+
+service homegear start
+service homegear-influxdb start
+tail -f /var/log/homegear/homegear.log
