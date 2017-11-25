bd704147879b915cee044e2376d25cabe72d40ed
Jan Schulz-Hofen <jan@plan.io>
2017-11-25 19:43:36 +0100

    Map all volumes to Hassio folders

--- Homegear/start.sh
+++ Homegear/start.sh
@@ -1,5 +1,12 @@
 #/bin/bash
 
+mkdir -p /config/homegear /share/homegear/lib /share/homegear/log
+chown homegear:homegear /config/homegear /share/homegear/lib /share/homegear/log
+rm -Rf /etc/homegear /var/lib/homegear /var/log/homegear
+ln -nfs /config/homegear     /etc/homegear
+ln -nfs /share/homegear/lib /var/lib/homegear
+ln -nfs /share/homegear/log /var/log/homegear
+
 if ! [ "$(ls -A /etc/homegear)" ]; then
 	cp -R /etc/homegear.config/* /etc/homegear/
 fi
